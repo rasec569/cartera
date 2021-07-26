f8ba6107239428d1fe5eeca5caabd3e100f00a55
rasec569 <cesaraugusto_569@hotmail.com>
2021-07-26 18:35:50 -0500

    conexion api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { AutenticacionGuard } from './guards/autenticacion.guard';
+import { RoleGuard } from './guards/role.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'inicio', component:InicioComponent},
+  { path:'privado', component:PrivadoComponent, canActivate:[AutenticacionGuard]},
+  { path:'admin', component:AdminComponent, canActivate:[RoleGuard] ,data:{expecteRole:'0'}},
+  { path: 'login', component: LoginComponent },
+  { path:'**', pathMatch:'full', redirectTo:'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+
+//Modulos
+import { FormsModule } from '@angular/forms';
+import{HttpClientModule}from '@angular/common/http';
+//Providers
+import{JwtHelperService, JWT_OPTIONS} from '@auth0/angular-jwt';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PrivadoComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide:JWT_OPTIONS, useValue:JWT_OPTIONS},
+    JwtHelperService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
