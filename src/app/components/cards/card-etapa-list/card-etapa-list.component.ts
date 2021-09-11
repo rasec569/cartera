05f859cf3d24839d3a110d1371304f740f5df01c
rasec569 <cesaraugusto_569@hotmail.com>
2021-09-11 13:56:27 -0500

    componente etapa

--- src/app/components/cards/card-etapa-list/card-etapa-list.component.ts
+++ src/app/components/cards/card-etapa-list/card-etapa-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EtapaService } from 'src/app/services/etapa.service';
+import { etapa } from 'src/app/Models/etapa.model'
+import { GlobalService } from 'src/app/providers/GlobalService';
+import { AlertService } from "../../_alert";
 
 @Component({
   templateUrl: './card-etapa-list.component.html',
@@ -6,9 +10,218 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardEtapaListComponent implements OnInit {
 
-  constructor() { }
+  Etapa:etapa={
+    id:"",
+    numero:"",
+    valor:"",
+    estado:"",
+    manzana:"",
+    idproyecto:"",
+    TIPO:"",
+    MENSAJE:""
+  }
+  @Input()
+  get color(): string {
+    return this._color;
+  }
+  set color(color: string) {
+    this._color = color !== "light" && color !== "dark" ? "light" : color;
+  }
+  private _color = "light";
+  listEtapa=[];
+  ClonelistEtapa=[];
+  idOption:number=1;
+  options = {
+    autoClose: true,
+    keepAfterRouteChange: false,
+  };
 
+  constructor(private EtapaS:EtapaService,
+    protected alertService: AlertService,
+    private globalEvents: GlobalService) { }
+
+    changeMode(option:number){
+      this.idOption=option;
+      if(option==1){
+        /* this.ListarEtapas(); */
+      }
+    }
   ngOnInit(): void {
+    /* this.ListarEtapas(); */
+  }
+  public showModal = false;
+  public toggleModal() {
+  this.showModal = !this.showModal;
+  }
+  clearDataEtapa(){
+    this.Etapa={
+      id:"",
+      numero:"",
+      valor:"",
+      estado:"",
+      manzana:"",
+      idproyecto:"",
+      TIPO:"",
+      MENSAJE:"",
+    }
+  }
+  validadorEtapa() {
+    if (
+      this.Etapa.numero.trim() == ""
+    ) {
+      this.alertService.warn("Todos los campos deben estar diligenciados!", this.options);
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  ListarEtapas(idProyecto:any):void{
+    this.Etapa.idproyecto=idProyecto;
+    try {
+      this.EtapaS.getEtapasProyecto(this.Etapa).subscribe(
+        (res: etapa[]) => {
+          if (res[0].TIPO == undefined && res[0].MENSAJE == undefined) {
+            this.listEtapa=res;
+            this.ClonelistEtapa=res;
+          } else {
+            this.alertService.error(res[0].MENSAJE, this.options);
+          }
+        },
+        (err) => {
+          this.alertService.error(
+            "Error de conexión, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+            this.options
+          );
+        }
+      );
+    } catch (error) {
+      this.alertService.error(
+        "Error de aplicación, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+        this.options
+      );
+    }
+  }
+  QueryOneEtapa(idProyecto: any) {
+    this.Etapa.id = idProyecto;
+    try {
+      this.EtapaS.getEtapa(this.Etapa).subscribe(
+        (res: etapa[]) => {
+          if (res[0].TIPO == undefined && res[0].MENSAJE == undefined) {
+            this.Etapa=res[0];
+            console.log(res[0])
+              this.changeMode(3);
+          } else {
+            this.alertService.error(res[0].MENSAJE, this.options);
+          }
+        },
+        (err) => {
+          this.alertService.error(
+            "Error de conexión, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+            this.options
+          );
+        }
+      );
+    } catch (error) {
+      this.alertService.error(
+        "Error de aplicación, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+        this.options
+      );
+    }
+  }
+  SaveEtapa() {
+    try {
+      if (this.validadorEtapa()) {
+        this.EtapaS.createEtapa(this.Etapa).subscribe(
+          (res: etapa[]) => {
+            if (res[0].TIPO == "3") {
+              this.alertService.success(res[0].MENSAJE, this.options);
+              this.changeMode(1);
+              this.QueryProyectos();
+              this.clearDataEtapa();
+            } else {
+              this.alertService.error(res[0].MENSAJE, this.options);
+            }
+          },
+          (err) => {
+            this.alertService.error(
+              "Error de conexión, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+              this.options
+            );
+          }
+        );
+      }
+    } catch (error) {
+      this.alertService.error(
+        "Error de aplicación, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+        this.options
+      );
+    }
+  }
+  UpdateEtapa() {
+    try {
+      if (this.validadorEtapa()) {
+        this.EtapaS.updateEtapa(this.Etapa).subscribe(
+          (res: etapa[]) => {
+            if (res[0].TIPO == "3") {
+              this.alertService.success(res[0].MENSAJE, this.options);
+              this.changeMode(1);
+              this.clearDataEtapa();
+            } else {
+              this.alertService.error(res[0].MENSAJE, this.options);
+            }
+          },
+          (err) => {
+            this.alertService.error(
+              "Error de conexión, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+              this.options
+            );
+          }
+        );
+      }
+    } catch (error) {
+      this.alertService.error(
+        "Error de aplicación, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+        this.options
+      );
+    }
+  }
+
+  RemoveEtapa(idProyecto: any) {
+    this.Etapa.id = idProyecto;
+    try {
+      this.EtapaS.deleteEtapa(this.Etapa).subscribe(
+        (res: etapa[]) => {
+          if (res[0].TIPO == "3") {
+            this.alertService.success(res[0].MENSAJE, this.options);
+            this.QueryProyectos();
+          } else {
+            this.alertService.error(res[0].MENSAJE, this.options);
+          }
+        },
+        (err) => {
+          this.alertService.error(
+            "Error de conexión, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+            this.options
+          );
+        }
+      );
+    } catch (error) {
+      this.alertService.error(
+        "Error de aplicación, trabajamos para habilitar el servicio en el menor tiempo posible, intentelo más tarde!",
+        this.options
+      );
+    }
+  }
+  async getItems(ev:any){
+    const val = ev.target.value;
+        if (val && val.trim() !== '') {
+          this.listEtapa = this.listEtapa.filter((item) => {
+            return (item.numero.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          });
+        } else {
+          this.listEtapa=this.ClonelistEtapa;
+        }
   }
 
 }
--- src/app/services/etapa.service.ts
+++ src/app/services/etapa.service.ts
@@ -36,6 +36,50 @@ public getEtapasProyecto(Etapa: etapa): Observable<any> {
     catchError(this.handleError)
   );
 }
+//buscar
+public getEtapa(Etapa: etapa): Observable<any> {
+  HttpOptionsBody.body.id=Etapa.id;
+  return this.http.get(`${environment.url}/etapa/${Etapa.id}`,HttpOptionsBody).pipe(
+    tap((result: any) => {
+    }),
+    catchError(this.handleError)
+  );
+}
+
+//registrar
+public createEtapa(Etapa:etapa): Observable<any> {
+  return this.http
+    .post(`${environment.url}/etapa/`, Etapa, httpOptions)
+    .pipe(
+      tap((result: any) => {
+        console.log(result);
+      }),
+      catchError(this.handleError)
+    );
+}
+//eliminar
+public deleteEtapa(Etapa: etapa): Observable<any> {
+  HttpOptionsBody.body.id=Etapa.id;
+  return this.http
+    .delete(`${environment.url}/etapa/`,HttpOptionsBody)
+    .pipe(
+      tap((result: any) => {
+        console.log(result);
+      }),
+      catchError(this.handleError)
+    );;
+}
+//modificar
+public updateEtapa(Etapa: etapa): Observable<any> {
+  return this.http
+    .put(`${environment.url}/etapa/${Etapa.id}`, Etapa, httpOptions)
+    .pipe(
+      tap((result: any) => {
+        console.log(result);
+      }),
+      catchError(this.handleError)
+    );
+}
 handleError(error: HttpErrorResponse) {
   let errorMessage = "Unknown error!";
   if (error.error instanceof ErrorEvent) {
