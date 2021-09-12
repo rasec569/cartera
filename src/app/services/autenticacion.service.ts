936c9acc54b0ac502df9c44aadb32fd8b838ea9c
Maicol Aguilar Perez <MitchAguilar@users.noreply.github.com>
2021-09-12 11:52:18 -0500

    update login

--- src/app/services/autenticacion.service.ts
+++ src/app/services/autenticacion.service.ts
@@ -1,19 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { tap, catchError } from "rxjs/operators";
 import { JwtHelperService } from '@auth0/angular-jwt';
-
+import { environment } from 'src/environments/environment';
+const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      "Content-Type": "application/json"
+    }
+  ),
+};
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacionService {
 
-  private URL='http://localhost:3000';
 
   constructor(private http:HttpClient,
     private jwtHelper:JwtHelperService) { }
 
-  signin(user:any){
-    return this.http.post(`${this.URL}/user/signin`, user);
+  signin(user:any): Observable<any>{
+    return this.http.post(`${environment.url}/login/`,user, httpOptions).pipe(
+      tap((result: any) => {
+      }),
+      catchError(this.handleError)
+    );
   }
   isAuth():boolean{
     const token:any= localStorage.getItem('token');
@@ -21,5 +34,16 @@ export class AutenticacionService {
       return false;
     }
     return true;
+  }
+
+  // error handle
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = "Unknown error!";
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
   }
 }
